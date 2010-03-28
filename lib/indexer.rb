a2dfdc4d34112cdb8172aacc74f03810ef919de2
Abhishek Parolkar <abhishek@parolkar.com>
2010-03-28 16:52:45 +0530

    Indexing

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -1,5 +1,19 @@
+require 'stemmer'
 class Indexer
- def index(content)
+ def self.index(content)
+   content_string = content.body
+   content_string.stemmed_words.each_with_index { |w, l|
+      rw= RanchoWord.find_or_create_by_stem(w)
+      rcp = RanchoContentPointer.find_or_create_by_content_id_and_content_type(content.id, content.class.to_s )
+      ri = RanchoIndex.new
+      ri.rancho_content_pointer = rcp
+      ri.rancho_word = rw
+      ri.position = l
+      ri.lat = content.location.lat
+      ri.lng = content.location.lng
+      ri.save
+      
+   }
    
 
  end 
--- migrations/02_create_rancho_index.rb
+++ migrations/02_create_rancho_index.rb
@@ -1,6 +1,6 @@
 class CreateRanchoIndex < ActiveRecord::Migration
   def self.up
-    create_table :rancho_indexes do |t|
+    create_table :rancho_indices do |t|
       t.references :rancho_content_pointer
       t.references :rancho_word
       t.integer :position       
@@ -9,12 +9,12 @@ class CreateRanchoIndex < ActiveRecord::Migration
 
       #t.timestamps
     end
-    add_index :rancho_indexes, :rancho_content_pointer_id
-    add_index :rancho_indexes, :rancho_word_id
-    add_index :rancho_indexes, [:rancho_word_id, :rancho_content_pointer_id]
+    add_index :rancho_indices, :rancho_content_pointer_id
+    add_index :rancho_indices, :rancho_word_id
+    add_index :rancho_indices, [:rancho_word_id, :rancho_content_pointer_id]
   end
 
   def self.down
-    drop_table :rancho_indexes
+    drop_table :rancho_indices
   end
 end
