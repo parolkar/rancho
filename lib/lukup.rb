3534a704c5829143a10f00697eafde856569ae55
Abhishek Parolkar <abhishek@parolkar.com>
2010-03-29 14:11:57 +0530

    Frequency ranking

--- lib/lukup.rb
+++ lib/lukup.rb
@@ -1,9 +1,21 @@
-#require 'stemmer'
+
 class Lukup
 
   
-  def search(for_text)
-    # do the basic work here and then call rank
+  def search(for_text)  
+        words_conditions = []
+        for_text.stemmed_words.each { |param| words_conditions << "stem = '#{param}'" }
+        word_sql = "select * from rancho_words where #{words_conditions.join(" or ")}"
+        @search_words = RanchoWord.find_by_sql(word_sql)
+        tables, joins, ids = [], [], []
+        @search_words.each_with_index { |w, index|
+          tables << "rancho_indices ri#{index}"
+          joins << "ri#{index}.rancho_content_pointer_id = ri#{index+1}.rancho_content_pointer_id"
+          ids << "ri#{index}.rancho_word_id = #{w.id}"
+        }
+        joins.pop
+        @common_select = "from #{tables.join(', ')} where #{(joins + ids).join(' and ')} group by ri0.rancho_content_pointer_id"
+        @search_words.empty? ? [] : rank()
   end
 
   def rank
@@ -11,10 +23,19 @@ class Lukup
   end
     
   def frequency_ranking
-   return {}
+    freq_sql= "select ri0.rancho_content_pointer_id, count(ri0.rancho_content_pointer_id) as count #{@common_select} order by count desc"
+    puts "Here is the query for frequency ranking \n #{freq_sql} \n"
+    list = RanchoIndex.find_by_sql(freq_sql)
+    rank = {}
+    list.size.times { |i| rank[list[i].rancho_content_pointer_id] = list[i].count.to_f/list[0].count.to_f }
+    return rank
   end  
   
   def word_distance_ranking
+    return {}
+  end     
+  
+  def geo_distance_ranking
     return {}
   end
   
